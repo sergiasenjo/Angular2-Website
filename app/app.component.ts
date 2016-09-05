a4d874dce1e3b4c6731a3f27bb487dcf0ac575dc
Sergio Asenjo <sergio.asenjo@novaquality.es>
2016-09-05 16:32:36 +0200

    Navbar initial implementation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar.component'
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>My First Angular 2 App</h1><navbar></navbar>'
 })
+
 export class AppComponent { }
---
+++ app/components/navbar.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'navbar',
+  template: '<div>Navbar</div>'
+})
+export class NavbarComponent { }
