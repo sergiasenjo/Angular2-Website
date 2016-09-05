f42b82a61d9f7b97bed01a57240d77591dc8018b
Sergio Asenjo <sergio.asenjo@novaquality.es>
2016-09-05 17:37:43 +0200

    Pages

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { HomeComponent } from './components/pages/home.component';
+import { AboutComponent } from './components/pages/about.component';
 
 @Component({
   selector: 'my-app',
-  template: '<navbar></navbar><jumbotron></jumbotron>'
+  template: '<navbar></navbar><jumbotron></jumbotron><home></home>'
 })
 
 export class AppComponent { }
